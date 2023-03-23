6d322ba2e5832cc6cb392175f0c3bcb18dc76b20
kitions <nsplknsplk@gmail.com>
2023-03-23 11:05:03 +0800

    fix: config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kitions/docusaurus-link-website/',
+            'https://github.com/kitions/docusaurus-link-website/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kitions/docusaurus-link-website/',
+            'https://github.com/kitions/docusaurus-link-website/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
